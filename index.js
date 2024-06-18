4e2b9b513fa460cfbdc360250e156b47469302d0
Abishek2022 <thapaabishek772@gmail.com>
2024-06-18 06:36:23 +0545

    first update

---
+++ index.js
@@ -0,0 +1,438 @@
+let data = document.getElementById('firsttext');
+let myinfo = [{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},
+
+{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},
+{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},
+{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},
+{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},
+{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},{
+    image: `p1.jpg`,
+    pname: `Samsung Galaxy s22 Ultra Pro(16*/256)| 6.76 FHD`,
+    storage:[
+{
+    title:"MEGA",
+},
+{
+    title:"Best Price",
+},
+    ],
+    offers:[
+        {
+            title:'Free Delivery'
+        },
+        {
+            title:'7 Voucher'
+        }
+    ],
+     price:`<div class="text-orange-500 font-bold text-3xl p-2 mt-3">RS. 1,59,900</div>`
+},
+
+];
+
+myinfo.map((val, index) => {
+    console.log(val.offers);
+    let news='';
+    val?.offers.map((item,ind)=>{
+        console.log(item)
+        news+=`<div>${item.title}</div>`
+    })
+
+    let storg='';
+    val?.storage.map((item,ind)=>{
+        console.log(item)
+        classn=`${ind==0?"bg-red-500":"bg-orange-500"} h-44 w-44 `
+        storg+=`<div class=${classn} >${item.title}</div>`
+    })
+
+    data.innerHTML += `
+        <div class='shadow-md'>
+            <div>
+            <img src=${val.image} />
+            </div>
+            <div class='flex items-center gap-4'>${storg}</div>
+            <div>${val.pname}</div>
+            <div>${news}</div>
+            <div>${val.price}</div>
+        </div>
+    `;
+});
